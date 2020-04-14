3bb8fc6b6916356b828427183d71515c76e10460
bluemania <damnthatswack@hotmail.com>
2020-04-14 10:04:11 -0400

    added historic auction scandata and inventory, added simple item valuation, adding code to evaluate current monies and inventory value

--- utils.py
+++ utils.py
@@ -6,13 +6,13 @@ from math import ceil
 from datetime import datetime as dt
 
     
-def merge(a, b, path=None):
+def source_merge(a, b, path=None):
     "merges b into a"
     if path is None: path = []
     for key in b:
         if key in a:
             if isinstance(a[key], dict) and isinstance(b[key], dict):
-                merge(a[key], b[key], path + [str(key)])
+                source_merge(a[key], b[key], path + [str(key)])
             elif a[key] == b[key]:
                 pass # same leaf value
             else:
@@ -22,7 +22,7 @@ def merge(a, b, path=None):
     return a
 
 
-def read_lua(datasource: str):
+def read_lua(datasource: str, merge_account_sources=True):
     """ Attempts to read lua from the given locations
     """
 
@@ -33,8 +33,10 @@ def read_lua(datasource: str):
         with open(path_live, 'r') as f:
             account_data[account_name] = lua.decode('{'+f.read()+'}')
 
-    return merge(account_data['BLUEM'], account_data['396255466#1'])
- 
+    if merge_account_sources:
+        return source_merge(account_data['BLUEM'], account_data['396255466#1'])
+    else:
+         return account_data
 
 
 def load_items():
@@ -56,9 +58,7 @@ def generate_inventory(verbose=False):
     Loads yaml files to specify item locations and specific items of interest
     Saves down parquet file ready to go
     """
-
     settings = get_general_settings() 
-    user_items = load_items()
     data = read_lua('ArkInventory')
 
     characters = data['ARKINVDB']['global']['player']['data'] 
@@ -104,31 +104,57 @@ def generate_inventory(verbose=False):
             character_inventories[character_name][settings['location_info'][lkey]] = items
 
     # Convert information to dataframe
-    cols = ['character', 'location', 'item', 'count']
-    df = pd.DataFrame(raw_data, columns=cols)
+    cols = ['character', 'location', 'item', 'count', 'timestamp']
+    df = pd.DataFrame(raw_data)
 
-    # Save out the monies information
-    total_monies = sum(list(monies.values()))/10000
+    df['timestamp'] = dt.now()
+    df.columns = cols
+    df.to_parquet('intermediate/inventory.parquet', compression='gzip')
+
+    df_monies = pd.Series(monies)
+    df_monies.name = 'monies'
+    df_monies = pd.DataFrame(df_monies)
+    df_monies['timestamp'] = dt.now()
+
+    df_monies.to_parquet('intermediate/monies.parquet', compression='gzip')
+
     if verbose:
-        print(f"Total Monies: {total_monies:.2f}")
-    with open('intermediate/monies.txt', 'w') as f:
-        f.write(str(total_monies))  
+        print(f"Inventory formatted. {len(df)} records, {int(df_monies['monies'].sum()/10000)} total money across chars")
+        
 
-    # Add label and stack size info and save
-    item_labels = {item: details['snatch_group'] for item, details in user_items.items()}
-    stack_sizes = {item: details.get('max_stack') for item, details in user_items.items()}
+    # Uncomment below for
+    # full scandata reset
 
-    df['snatch_group'] = df['item'].apply(lambda x: item_labels.get(x))
-    df['stack_size'] = df['item'].apply(lambda x: stack_sizes.get(x))
-    df.to_parquet('intermediate/inventory_full.parquet', compression='gzip')    
+    # cols = ['monies', 'timestamp']
+    # auction_scandata_reset = pd.DataFrame(columns=cols)
+    # auction_scandata_reset.to_parquet('full/monies.parquet', compression='gzip')
 
-    # Create more focused version
-    df_subset = df.dropna().copy()
-    df_subset['stack_size'] = df_subset['stack_size'].astype(int)
+    # cols = ['character', 'location', 'item', 'count', 'timestamp']
+    # auction_scandata_reset = pd.DataFrame(columns=cols)
+    # auction_scandata_reset.to_parquet('full/inventory.parquet', compression='gzip')
 
-    df_subset.to_parquet('intermediate/inventory.parquet', compression='gzip')
+    # Uncomment above for
+    # full scandata reset  
+
+    inventory_repo = pd.read_parquet('full/inventory.parquet')
+    inventory_repo.to_parquet('full_backup/inventory.parquet', compression='gzip')
+
+    monies_repo = pd.read_parquet('full/monies.parquet')
+    monies_repo.to_parquet('full_backup/monies.parquet', compression='gzip')
+
+    updated = '*not*'
+    if df['timestamp'].max() > inventory_repo['timestamp'].max():
+        updated = ''
+        inventory_repo = inventory_repo.append(df)
+        inventory_repo.to_parquet('full/inventory.parquet', compression='gzip')
+
+        monies_repo = monies_repo.append(df_monies)
+        monies_repo.to_parquet('full/monies.parquet', compression='gzip')    
+
+    unique_periods = len(inventory_repo['timestamp'].unique())
+
     if verbose:
-        print(f'Saving inventory data with {df_subset.shape} shape')
+        print(f"Inventory full repository. {len(inventory_repo)} records with {unique_periods} snapshots. Repository has {updated} been updated this run")
 
 
 def get_character_needs(character=[]):
@@ -191,8 +217,89 @@ def get_mule_counts():
     item_count = item_count[['category','count']]
     return item_count
 
+def format_auction_data(ac):
+    """ Reads the raw scandata dict dump and converts to usable dataframe    
+    """
+    auction_data = []
 
-def generate_auction_history(verbose=False):
+    for rope in ac['AucScanData']['scans']['Grobbulus']['ropes']:
+        auctions = rope[9:-3].split('},{')
+        for auction in auctions:
+            auction_data.append(auction.split('|')[-1].split(','))
+
+    # Contains lots of columns, we ignore ones we likely dont care about
+    # We apply transformations and relabel
+    df = pd.DataFrame(auction_data)
+    df['item'] = df[8].str.replace('"','')
+    df['count'] = df[10].replace('nil', 0).astype(int)
+    df['price'] = df[16].astype(int)
+    df['agent'] = df[19].str.replace('"','')
+    df['timestamp'] = df[7].apply(lambda x: dt.fromtimestamp(int(x)))
+
+    # There is some timing difference in the timestamp, we dont really care we just need time of pull
+    df['timestamp'] = df['timestamp'].max()
+
+    df = df[df['count']>0]
+    df['price_per'] = df['price'] / df['count']
+
+    cols = ["timestamp", "item", "count", "price", "agent", "price_per"]
+    df = df[cols]
+
+    return df
+
+
+def generate_auction_scandata(verbose=False):
+    """ Snapshot of all AH prices from latest scan
+        Reads the raw scandata from both accounts, cleans and pulls latest only
+        Saves latest scandata to intermediate and adds to a full database with backup
+    """
+    raw_scan_data = read_lua('Auc-ScanData', merge_account_sources=False)
+
+    cleaned_scan_data = {account: format_auction_data(auction_data) for account, auction_data in raw_scan_data.items()}
+
+    latest_account_access = None
+    latest_account_time = pd.to_datetime('1985-05-15')
+    for account, auction_data in cleaned_scan_data.items():
+        if auction_data['timestamp'].max() > latest_account_time:
+            latest_account_time = auction_data['timestamp'].max()
+            latest_account_access = account
+
+    auction_data = cleaned_scan_data[latest_account_access]
+
+    # Saves latest scan to intermediate (immediate)
+    auction_data.to_parquet('intermediate/auction_scandata.parquet', compression='gzip')
+    
+    if verbose:
+        print(f"Auction scandata loaded and cleaned. {len(auction_data)} records, last updated {latest_account_time} by {latest_account_access}")
+
+    # Uncomment below for
+    # full scandata reset
+
+    # # # ensure cols are same as those used in clean
+    #cols = ["timestamp", "item", "count", "price", "agent", "price_per"]
+    #auction_scandata_reset = pd.DataFrame(columns=cols)
+    #auction_scandata_reset.to_parquet('full/auction_scandata.parquet', compression='gzip')
+    
+    # Uncomment above for
+    # full scandata reset
+
+    # Saves full backup and adds latest to full
+    auction_data_repo = pd.read_parquet('full/auction_scandata.parquet')
+    auction_data_repo.to_parquet('full_backup/auction_scandata.parquet', compression='gzip')
+    
+    updated = '*not*'
+    if auction_data['timestamp'].max() > auction_data_repo['timestamp'].max():
+        updated = ''
+        auction_data_repo = auction_data_repo.append(auction_data)
+        auction_data_repo.to_parquet('full/auction_scandata.parquet', compression='gzip')
+    
+    unique_periods = len(auction_data_repo['timestamp'].unique())
+    
+    if verbose:
+        print(f"Auction scandata full repository. {len(auction_data_repo)} records with {unique_periods} snapshots. Repository has {updated} been updated this run")
+
+
+def generate_auction_activity(verbose=False):
     """ Generates auction history parquet file with auctions of interest.
         Reads and parses Beancounter auction history across all characters
         Works the data into a labelled and cleaned pandas before parquet saves
@@ -234,19 +341,19 @@ def generate_auction_history(verbose=False):
     df['timestamp'] = df['timestamp'].apply(lambda x: dt.fromtimestamp(int(x)))
 
     if verbose:
-        print(f"{df.shape[0]} auction events")
-    df.to_parquet('intermediate/auctions_full.parquet', compression='gzip')
+        print(f"Auction actions full repository. {df.shape[0]} records")
+    df.to_parquet('full/auction_activity.parquet', compression='gzip')
 
-    user_items = load_items()
-    item_labels = {item: details['snatch_group'] for item, details in user_items.items()}
+    # user_items = load_items()
+    # item_labels = {item: details['snatch_group'] for item, details in user_items.items()}
 
-    df_interest = df.loc[df[df['item'].isin(user_items)].index]
-    df_interest['snatch_group'] = df_interest['item'].replace(item_labels)
+    # df_interest = df.loc[df[df['item'].isin(user_items)].index]
+    # df_interest['snatch_group'] = df_interest['item'].replace(item_labels)
 
-    if verbose:
-        print(f"{df_interest.shape[0]} auction events of interest")
+    # if verbose:
+    #     print(f"{df_interest.shape[0]} auction events of interest")
 
-    df_interest.sort_values('timestamp').to_parquet('intermediate/auctions.parquet', compression='gzip')
+    # df_interest.sort_values('timestamp').to_parquet('intermediate/auctions.parquet', compression='gzip')
 
 
 def generate_historic_average_prices(verbose=False, MAX_RECENT=25, MIN_RECENT=5, MAX_SUCCESS=250, MIN_SUCCESS=10, MIN_PROFIT_MARGIN=1000):
@@ -257,7 +364,7 @@ def generate_historic_average_prices(verbose=False, MAX_RECENT=25, MIN_RECENT=5,
     """
 
     user_items = load_items()
-    df_raw = pd.read_parquet('intermediate/auctions.parquet')
+    df_raw = pd.read_parquet('full/auction_actions.parquet')
     df = df_raw.copy()
 
     df['rank'] = df.groupby(['auction_type', 'item'])['timestamp'].rank(ascending=False)

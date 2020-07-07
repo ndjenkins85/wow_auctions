bdd08b8f7b302a80933c9cac442784e5f6b3bc4f
bluemania <damnthatswack@hotmail.com>
2020-07-07 10:58:20 -0400

    completed conversion to library file structure

--- run.py
+++ run.py
@@ -6,29 +6,29 @@ import click
 # Import logging with dt
 # Try packaging the files
 # Remove parquet from git
+# Poetry
 
-from sources import *
-from analysis import * 
+from pricer import sources, analysis
 from datetime import datetime as dt
 
 
 def analyse():
 	""" Load sources, calculate prices, create policies
 	"""
-	generate_booty_data(verbose=True)
-	generate_auction_scandata(verbose=True)
-	generate_auction_activity(verbose=True)
-	generate_inventory(verbose=True)
-	analyse_item_prices(verbose=True)
-	analyse_sales_performance()
-	analyse_item_min_sell_price(MAT_DEV=0)
-	analyse_sell_data()
-	apply_buy_policy(MAT_DEV=0)
+	sources.generate_booty_data(verbose=True)
+	sources.generate_auction_scandata(verbose=True)
+	sources.generate_auction_activity(verbose=True)
+	sources.generate_inventory(verbose=True)
+	analysis.analyse_item_prices(verbose=True)
+	analysis.analyse_sales_performance()
+	analysis.analyse_item_min_sell_price(MAT_DEV=0)
+	analysis.analyse_sell_data()
+	analysis.apply_buy_policy(MAT_DEV=0)
 
 #apply_sell_policy(stack_size=5, leads_wanted=20, duration='medium', update=True)
 #apply_sell_policy(stack_size=1, leads_wanted=25, duration='medium', update=True, leave_one=False)
 #apply_sell_policy(stack_size=5, leads_wanted=50, duration='long', update=True, factor=2)
-
+#generate_new_pricer_file
 
 if __name__ == "__main__":
 	start = dt.now()
